e796cc9b74b20fe201fef178f7739903dec0fd92
ivansoto <lopezsotoivan01@gmail.com>
2022-05-24 19:25:13 -0500

    first commit

---
+++ src/test/java/com/curame/finanzas/FinanzasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curame.finanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
